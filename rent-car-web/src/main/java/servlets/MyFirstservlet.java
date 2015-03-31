1d3de7e690d1a9a44a28acaca34ac5907344b8b2
medali.bettaieb <medali.bettaieb@esprit.tn>
2015-03-31 17:14:30 +0100

    servlet
    Signed-off-by: medali.bettaieb <medali.bettaieb@esprit.tn>

---
+++ rent-car-web/src/main/java/servlets/MyFirstservlet.java
@@ -0,0 +1,62 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyFirstservlet
+ */
+@WebServlet("/MyFirstservlet")
+public class MyFirstservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MyFirstservlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter writer = response.getWriter();
+		Date date = new Date();
+		String name = request.getParameter("n");
+		SimpleDateFormat format;
+		writer.write("<html><body><h1>hello " + name + " it is :" + date
+				+ " </h1></body></html>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
