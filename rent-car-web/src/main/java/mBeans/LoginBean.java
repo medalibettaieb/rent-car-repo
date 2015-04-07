89b1133ac1499e51b1239f14ac50664ef188a560
medali.bettaieb <medali.bettaieb@esprit.tn>
2015-04-07 16:43:39 +0100

    login static
    Signed-off-by: medali.bettaieb <medali.bettaieb@esprit.tn>

---
+++ rent-car-web/src/main/java/mBeans/LoginBean.java
@@ -0,0 +1,42 @@
+package mBeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+
+	private String login;
+	private String password;
+
+	public String doLogin() {
+		String navigateTo = "";
+		if (login.equalsIgnoreCase("esprit")
+				&& password.equalsIgnoreCase("esprit")) {
+			navigateTo = "/pages/success?faces-redirect=true";
+		} else {
+			navigateTo = "/error?faces-redirect=true";
+		}
+
+		return navigateTo;
+
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
---
+++ rent-car-web/src/main/java/mBeans/MrBean.java
@@ -0,0 +1,33 @@
+package mBeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class MrBean {
+	private String nameOne;
+	private String nameTow;
+
+	public String doSomeStuff() {
+		nameTow = nameOne;
+		return "/login";
+	}
+
+	public String getNameOne() {
+		return nameOne;
+	}
+
+	public void setNameOne(String nameOne) {
+		this.nameOne = nameOne;
+	}
+
+	public String getNameTow() {
+		return nameTow;
+	}
+
+	public void setNameTow(String nameTow) {
+		this.nameTow = nameTow;
+	}
+
+}
