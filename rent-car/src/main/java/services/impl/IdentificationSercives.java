ec76b4b76b6f7ff7e2574d8656765e066f66d230
medali.bettaieb <medali.bettaieb@esprit.tn>
2015-04-07 15:21:56 +0100

    identification
    Signed-off-by: medali.bettaieb <medali.bettaieb@esprit.tn>

---
+++ rent-car/src/main/java/services/impl/IdentificationSercives.java
@@ -0,0 +1,20 @@
+package services.impl;
+
+import javax.ejb.Stateless;
+import services.interfaces.IdentificationSercivesLocal;
+import services.interfaces.IdentificationSercivesRemote;
+
+/**
+ * Session Bean implementation class IdentificationSercives
+ */
+@Stateless
+public class IdentificationSercives implements IdentificationSercivesRemote, IdentificationSercivesLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public IdentificationSercives() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
